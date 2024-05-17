a77adaaef04c3d21afca58fcc93b8b31c9d231a5
rezarinaldi <rez4rinaldi@gmail.com>
2024-05-17 21:41:37 +0700

    feat: adding favicon

--- src/views/templates/baseHtml.tsx
+++ src/views/templates/baseHtml.tsx
@@ -2,9 +2,10 @@ export const BaseHTML = ({ children }: Html.PropsWithChildren) => {
   return (
     <html>
       <head>
-        <title>Bexlite</title>
+        <title>Template Bexlite Stack</title>
         <script src="https://unpkg.com/htmx.org@1.9.12"></script>
         <script src="https://unpkg.com/htmx.org@1.9.12/dist/ext/disable-element.js"></script>
+        <link rel="icon" type="image/png" href="/public/favicon.png" />
         <link rel="stylesheet" href="/public/globals.css" />
       </head>
       <body>{children}</body>
